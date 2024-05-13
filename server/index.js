b6613f61c4f6d4a356b1412a584e2a998724e501
Ishan <ishan.191618#ncit.edu.np>
2024-05-13 16:06:56 +0545

    Khalti payment detail saving api made and web socket initialized.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const http = require("http");
+const WebSocket = require("ws");
 const { PORT } = require("./config");
 
 // connect to db
@@ -8,8 +9,19 @@ require("./dbConnection/dbConnection").connect();
 // creating server
 const server = http.createServer(app);
 
+// Create WebSocket server
+const wss = new WebSocket.Server({ server: server });
 
+wss.on("connection", (ws) => {
+  console.log("New Client connected");
+  ws.send("Welcome to Udharo Server");
 
-server.listen(process.env.PORT, () =>
+  ws.on("message", function incomig(message) {
+    console.log("received: %s", message);
+    ws.send(`You sent -> ${message}`);
+  });
+});
+
+server.listen(PORT, () =>
   console.log(`Udharo Server is running on port:${PORT}`)
 );
--- server/models/khaltiPaymentModel.js
+++ server/models/khaltiPaymentModel.js
@@ -1,5 +1,13 @@
-const mongoose = requrie("mongoose");
+const mongoose = require("mongoose");
 
-const khaltiPaymentDetails = new mongoose.Schema({});
+const khaltiPaymentDetails = new mongoose.Schema({
+  idx: String,
+  amount: Number,
+  mobile: String,
+  product_identity: String,
+  product_name: String,
+  product_url: String,
+  token: String,
+});
 
 module.exports = khaltiPaymentDetails;
